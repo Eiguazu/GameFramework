be0a11ea4ce8a6774238e2c36164cd447d406a33
Eiguazu <ez2604@nyu.edu>
2026-09-21 04:01:31 -0400

    Minor changes, final stuff for today

--- GameFramework/Game/Main.cs
+++ GameFramework/Game/Main.cs
@@ -11,6 +11,8 @@ namespace GameFramework
     {
         public void Update(GameTime gameTime)
         { 
+            // Tbh main is not really nececassry, as you graduated to just using gameobjects. Most of what is going to go into the "Game" folder are those objects such 
+            // as player, enemy, bullet, etc. Geninely everything becomes super simple. 
         }
     }
 }
